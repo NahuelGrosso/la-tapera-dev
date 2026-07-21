2dbd2fe3ffc13c4fa4e44fb6a305e6c75ff62735
NahuelGrosso <nahuelgrosso1982@gmail.com>
2026-07-21 20:52:09 -0300

    Seccion nuestra historia casi lista

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
-const header = document.querySelector("header");
+/* ==========================================
+   LA TAPERA DEV
+   Archivo: main.js
 
-window.addEventListener("scroll", () => {
+   Funcionalidades JavaScript del sitio.
+========================================== */
 
-    if (window.scrollY > 40) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+/* ==========================================
+   SHRINK A HERO SCROLL
+========================================== */
+/*     */
+const hero = document.querySelector("#hero");
 
-});
+/* Alturas del Hero */
+const HERO_MAX_HEIGHT = 100;
+const HERO_MIN_HEIGHT = 80;
+
+/* Distancia de scroll necesaria para completar la animación */
+const SCROLL_DISTANCE = 300;
+
+window.addEventListener("scroll", heroCollapse);
+
+function heroCollapse() {
+
+    const scroll = window.scrollY;
+
+    /* Calculamos el progreso (0 → 1) */
+
+    const progress = Math.min(scroll / SCROLL_DISTANCE, 1);
+
+    /* Calculamos la altura */
+
+    const currentHeight =
+        HERO_MAX_HEIGHT -
+        ((HERO_MAX_HEIGHT - HERO_MIN_HEIGHT) * progress);
+
+    hero.style.height = `${currentHeight}vh`;
+
+}
